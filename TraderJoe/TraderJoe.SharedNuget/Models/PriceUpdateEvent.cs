b351d6bcc6fb2629a3d896f8623f4b2103207062
grace-kal <kalinina.grace@gmail.com>
2026-06-04 15:31:59 +0300

    Nuget: add price updated event model

---
+++ TraderJoe/TraderJoe.SharedNuget/Models/PriceUpdateEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace TraderJoe.SharedNuget.Models;
+
+public class PriceUpdateEvent
+{
+    public string Symbol { get; init; } = string.Empty;
+    public decimal BidPrice { get; init; }
+    public decimal AskPrice { get; init; }
+    public DateTime Timestamp { get; init; }
+}
