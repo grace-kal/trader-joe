c2d28bd58a5afb22dca457dc619bc47a6a535862
grace-kal <kalinina.grace@gmail.com>
2026-06-04 19:46:48 +0300

    created - publisher(done, not tested)

---
+++ TraderJoe/TraderJoe.PricingEngine/Publisher/IPricePublisher.cs
@@ -0,0 +1,8 @@
+﻿using TraderJoe.SharedNuget.Models;
+
+namespace TraderJoe.PricingEngine.Publisher;
+
+public interface IPricePublisher
+{
+    ValueTask PublishAsync(PriceUpdateEvent priceUpdate, CancellationToken ct = default);
+}
---
+++ TraderJoe/TraderJoe.PricingEngine/Publisher/PricePublisher.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Channels;
+using TraderJoe.SharedNuget.Models;
+
+namespace TraderJoe.PricingEngine.Publisher;
+
+public class PricePublisher(ChannelWriter<PriceUpdateEvent> writer) : IPricePublisher
+{
+    public async ValueTask PublishAsync(PriceUpdateEvent priceUpdate, CancellationToken ct = default)
+    {
+        await writer.WriteAsync(priceUpdate, ct);
+    }
+}
